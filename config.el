66e4aa33238df0b999316b5db19e024cfd96f8bb
William Marty Buchaus <marty@dabuke.com>
2021-09-20 11:43:29 -0400

    Update config.el, config.org, and 2 more files...

--- config.el
+++ config.el
@@ -626,37 +626,6 @@
   (add-hook 'doom-first-buffer-hook  #'marty/startWakatime)
   (setq wakatime-cli-path "/usr/bin/wakatime"))
 
-(use-package! tree-sitter
-  :when (bound-and-true-p module-file-suffix)
-  :hook (prog-mode . turn-on-tree-sitter-mode)
-  :hook (tree-sitter-after-on . tree-sitter-hl-mode)
-  :config
-  (require 'tree-sitter-langs)
-
-  ;; This makes every node a link to a section of code
-  (setq tree-sitter-debug-jump-buttons t
-        ;; and this highlights the entire sub tree in your code
-        tree-sitter-debug-highlight-jump-region t))
-
-(when (featurep! :editor evil +everywhere)
-  (use-package! evil-textobj-tree-sitter
-    :after tree-sitter
-    :config
-    (map!
-     :textobj "f" nil nil
-     :textobj "f" (evil-textobj-tree-sitter-get-textobj "function.inner") (evil-textobj-tree-sitter-get-textobj "function.outer") ;; redef
-
-     :textobj "C" (evil-textobj-tree-sitter-get-textobj "class.inner") (evil-textobj-tree-sitter-get-textobj "class.outer")
-
-     :textobj "c" nil nil
-     :textobj "c" nil (evil-textobj-tree-sitter-get-textobj "comment.outer")
-
-     :textobj "i" nil nil
-     :textobj "i" (evil-textobj-tree-sitter-get-textobj "conditional.inner") (evil-textobj-tree-sitter-get-textobj "conditional.outer")
-
-     :textobj "l" nil nil
-     :textobj "l" (evil-textobj-tree-sitter-get-textobj "loop.inner") (evil-textobj-tree-sitter-get-textobj "loop.outer"))))
-
 ;;;; Custom
 
 (setq-default custom-file (expand-file-name ".custom.el" doom-private-dir))
--- packages.el
+++ packages.el
@@ -81,13 +81,3 @@
                                                  :files ("*.el") ))
 
 (package! wakatime-mode)
-
-(package! tree-sitter
-  :pin "4d9871d23999fe5f8de821e23c9ec576df2b2738")
-
-(package! tree-sitter-langs
-  :pin "fa47b55f7bd11bd2b17ab48deb03ed23000bb974")
-
-(when (featurep! :editor evil +everywhere)
-  (package! evil-textobj-tree-sitter
-    :pin "eedc1f54611e4403ea228b33056388a8539a2b3e"))
