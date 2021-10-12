2b2735925a875c43c84bfcbaeed3e6bb40cee666
Marty Buchaus <marty@dabuke.com>
2021-10-12 15:25:36 -0400

    after fix up

--- config.el
+++ config.el
@@ -66,6 +66,10 @@
 
 (add-hook! 'org-mode-hook #'mixed-pitch-mode)
 
+;; additional colors
+(setq +m-color-main "#61AFEF"
+      +m-color-secondary "red")
+
 ;;;;; THEME
 (setq doom-theme 'doom-dracula )
 
@@ -109,9 +113,53 @@
 
 ;;;; SPELLING
 
-(after! flyspell
-        (setq flyspell-lazy-idle-seconds 2)
-        (setq ispell-personal-dictionary (expand-file-name ".ispell_personal" doom-private-dir)))
+(use-package flyspell
+  :defer 7
+  :config
+  ;; (setq ispell-program-name "aspell")
+  ;; You could add extra option "--camel-case" for since Aspell 0.60.8
+  ;; @see https://github.com/redguardtoo/emacs.d/issues/796
+  ;; (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together" "--run-together-limit=16"))
+  (setq-default flyspell-prog-text-faces
+                '(tree-sitter-hl-face:comment
+                  tree-sitter-hl-face:doc
+                  tree-sitter-hl-face:string
+                  tree-sitter-hl-face:function
+                  tree-sitter-hl-face:variable
+                  tree-sitter-hl-face:type
+                  tree-sitter-hl-face:method
+                  tree-sitter-hl-face:function.method
+                  tree-sitter-hl-face:function.special
+                  tree-sitter-hl-face:attribute
+                  font-lock-comment-face
+                  font-lock-doc-face
+                  font-lock-string-face
+                  lsp-face-highlight-textual
+                  default))
+
+
+  (setq flyspell-lazy-idle-seconds 2)
+  (setq ispell-personal-dictionary (expand-file-name ".ispell_personal" doom-private-dir))
+  (setq spell-fu-directory "~/.config/doom/dictionary") ;; Please create this directory manually.
+  (after! ispell
+    (setq ispell-program-name "aspell"
+          ;; Notice the lack of "--run-together"
+          ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together" "--run-together-limit=16"))
+    (ispell-kill-ispell t))
+
+  (defun flyspell-buffer-after-pdict-save (&rest _)
+    (flyspell-buffer))
+
+  (advice-add 'ispell-pdict-save :after #'flyspell-buffer-after-pdict-save))
+
+
+;; (use-package spell-fu
+;;   :defer 0.1
+;;   :config
+;;   (global-spell-fu-mode))
+
+(add-hook 'text-mode-hook 'flyspell-mode!)
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
 ;;;; KEYBINDINGS
 
@@ -196,7 +244,7 @@
             (define-auto-insert "daily/[^/].+\\.org?$" ["defaultRoamDaily.org" marty/autoinsert-yas-expand])
             (define-auto-insert "/[0-9]\\{8\\}.org$" ["defaultJournal.org" marty/autoinsert-yas-expand])))
 
-;;;; FUNCTIONS
+;;;;; FUNCTIONS
 
 (defun marty/autoinsert-yas-expand ()
   "This is used by the autoinsert package to grab and expand
@@ -227,6 +275,36 @@ templates into newly created files"
 
   )
 
+;;;; COMPANY
+
+(defun my-setup-tabnine ()
+  (interactive)
+  (setq-local company-backends '(company-tabnine :separate company-capf)))
+
+;;;;; Only tabnine
+(defun my-setup-tabnine-2 ()
+  (interactive)
+  (setq-local company-backends '(company-tabnine)))
+
+(defun my-setup-tabnine-3 ()
+  (interactive)
+  (setq-local +lsp-company-backends '((company-capf)))
+  (setq-local company-backends '((company-capf))))
+
+;; (my-setup-tabnine)
+;; Autocomplete with AI
+(use-package company-tabnine
+  :after (company lsp)
+  :bind (("C-x C-i" . company-tabnine))
+  :when (featurep! :completion company)
+  :config
+  (setq company-idle-delay 0.1)
+  (setq company-show-numbers nil)
+  (setq company-minimum-prefix-length 1)
+  (setq company-tabnine-show-annotation t)
+  (setq company-dabbrev-char-regexp "[A-z:-]"))
+
+;;;;
 ;;;; COUNSEL TRAMP
 
 (use-package! counsel-tramp
@@ -423,6 +501,16 @@ templates into newly created files"
   (dashboard-modify-heading-icons '((recents . "file-text")
                                     (bookmarks . "book"))))
 
+;;;; DOCKER COMPOSE
+
+(use-package docker-compose-mode
+  :defer 6)
+
+;;;; DOCKER
+
+(use-package dockerfile-mode
+  :defer 6)
+
 ;;;; EBUKU
 ;;
 ;; Add the E-buku management mode for buku
@@ -446,9 +534,144 @@ templates into newly created files"
         "e" #'elfeed)
   (elfeed-protocol-enable))
 
+;;;; ELISP
+
+(use-package elisp-mode
+  :defer 4
+  :bind (("C-c o" . outline-cycle)
+         ("C-c r" . outline-show-all)
+         ("C-c m" . outline-hide-body)
+         ("C-c ]" . outline-next-heading)
+         ("C-c [" . outline-previous-heading)
+         ("C-c c" . counsel-outline)
+         ("C-c e" . outline-hide-entry)
+         ("C-c t" . outline-toggle-children)
+         ("C-c b" . outline-cycle-buffer)))
+
+;;;; GIT
+;;;;; MAGIT
+
+(use-package magit
+  :defer 0.3
+  :config
+  (define-key transient-map        "q" 'transient-quit-one)
+  (define-key transient-edit-map   "q" 'transient-quit-one)
+  (define-key transient-sticky-map "q" 'transient-quit-seq))
+
+;;;;; GIT-GUTTER
+
+(use-package git-gutter
+  :defer 3
+  :init
+  (global-git-gutter-mode)
+  (global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+  (global-set-key (kbd "C-x n") 'git-gutter:next-hunk))
+
+;;;;; GIT MESSENGER
+
+(use-package git-messenger
+  :defer 25
+  :bind (:map vc-prefix-map
+         ("p" . git-messenger:popup-message)
+         :map git-messenger-map
+         ("m" . git-messenger:copy-message))
+  :config
+  (setq git-messenger:show-detail t
+        git-messenger:use-magit-popup t)
+  ;; :config
+  (with-no-warnings
+    (with-eval-after-load 'hydra
+      (defhydra git-messenger-hydra (:color blue)
+        ("s" git-messenger:popup-show "show")
+        ("c" git-messenger:copy-commit-id "copy hash")
+        ("m" git-messenger:copy-message "copy message")
+        ("," (catch 'git-messenger-loop (git-messenger:show-parent)) "go parent")
+        ("q" git-messenger:popup-close "quit")))
+
+    (defun my-git-messenger:format-detail (vcs commit-id author message)
+      (if (eq vcs 'git)
+          (let ((date (git-messenger:commit-date commit-id))
+                (colon (propertize ":" 'face 'font-lock-comment-face)))
+            (concat
+             (format "%s%s %s \n%s%s %s\n%s  %s %s \n"
+                     (propertize "Commit" 'face 'font-lock-keyword-face) colon
+                     (propertize (substring commit-id 0 8) 'face 'font-lock-comment-face)
+                     (propertize "Author" 'face 'font-lock-keyword-face) colon
+                     (propertize author 'face 'font-lock-string-face)
+                     (propertize "Date" 'face 'font-lock-keyword-face) colon
+                     (propertize date 'face 'font-lock-string-face))
+             (propertize (make-string 38 ?─) 'face 'font-lock-comment-face)
+             message
+             (propertize "\nPress q to quit" 'face '(:inherit (font-lock-comment-face italic)))))
+        (git-messenger:format-detail vcs commit-id author message)))
+
+    (defun my-git-messenger:popup-message ()
+      "Popup message with `posframe', `pos-tip', `lv' or `message', and dispatch actions with `hydra'."
+      (interactive)
+      (let* ((vcs (git-messenger:find-vcs))
+             (file (buffer-file-name (buffer-base-buffer)))
+             (line (line-number-at-pos))
+             (commit-info (git-messenger:commit-info-at-line vcs file line))
+             (commit-id (car commit-info))
+             (author (cdr commit-info))
+             (msg (git-messenger:commit-message vcs commit-id))
+             (popuped-message (if (git-messenger:show-detail-p commit-id)
+                                  (my-git-messenger:format-detail vcs commit-id author msg)
+                                (cl-case vcs
+                                  (git msg)
+                                  (svn (if (string= commit-id "-")
+                                           msg
+                                         (git-messenger:svn-message msg)))
+                                  (hg msg)))))
+        (setq git-messenger:vcs vcs
+              git-messenger:last-message msg
+              git-messenger:last-commit-id commit-id)
+        (run-hook-with-args 'git-messenger:before-popup-hook popuped-message)
+        (git-messenger-hydra/body)
+        (cond ((and (fboundp 'posframe-workable-p) (posframe-workable-p))
+               (let ((buffer-name "*git-messenger*"))
+                 (posframe-show buffer-name
+                                :string popuped-message
+                                :left-fringe 8
+                                :right-fringe 8
+                                ;; :poshandler #'posframe-poshandler-window-top-right-corner
+                                :poshandler #'posframe-poshandler-window-top-right-corner
+                                ;; Position broken with xwidgets and emacs 28
+                                ;; :position '(-1 . 0)
+                                :y-pixel-offset 20
+                                :x-pixel-offset -20
+                                :internal-border-width 2
+                                :lines-truncate t
+                                :internal-border-color (face-foreground 'font-lock-comment-face)
+                                :accept-focus nil)
+                 (unwind-protect
+                     (push (read-event) unread-command-events)
+                   (posframe-delete buffer-name))))
+              ((and (fboundp 'pos-tip-show) (display-graphic-p))
+               (pos-tip-show popuped-message))
+              ((fboundp 'lv-message)
+               (lv-message popuped-message)
+               (unwind-protect
+                   (push (read-event) unread-command-events)
+                 (lv-delete-window)))
+              (t (message "%s" popuped-message)))
+        (run-hook-with-args 'git-messenger:after-popup-hook popuped-message)))
+    (advice-add #'git-messenger:popup-close :override #'ignore)
+    ;; (advice-add #'git-messenger:popup-close :override #'(setq modal-opened 0))
+    (advice-add #'git-messenger:popup-message :override #'my-git-messenger:popup-message)));;;
+;;;; Hydra
+
+(use-package hydra
+  :defer 8)
+
 ;;;; I3 WINDOW MANAGER CONFIG
 ;; Syntax highlighting for i3 config
 (use-package! i3wm-config-mode)
+
+;;;; JENKINS
+
+(use-package jenkinsfile-mode
+  :defer 6)
 
 ;;;; KHARD
 
@@ -533,6 +756,15 @@ templates into newly created files"
 
 (setq magit-revision-show-gravatars '("^author:     " . "^commit:     "))
 
+;;;; NGINX
+
+(use-package company-nginx
+  :after nginx-mode
+  :config (add-hook 'nginx-mode-hook (lambda () (add-to-list 'company-backends #'company-nginx))))
+
+(use-package nginx-mode
+  :defer 10)
+
 ;;;; MUTT-MODE
 
 (use-package! mutt-mode)
@@ -578,6 +810,27 @@ templates into newly created files"
         "f"  #'paperless-file
         "x"  #'paperless-execute))
 
+;;;; PYTHON MODE
+
+(use-package python-mode
+  :defer 0.5
+  :hook (python-mode . format-all-mode)
+  :config
+  (setq pytnon-indent-level 4)
+  (add-hook 'python-mode-hook
+            (lambda ()
+              (setq indent-tabs-mode nil)
+              (setq tab-width 4))))
+
+;;;;; LSP PYRIGHT
+
+(use-package lsp-pyright
+  :defer 0.5
+  :config
+  (setq lsp-pyright-auto-import-completions t)
+  (setq lsp-pyright-auto-search-paths t)
+  (setq lsp-pyright-log-level "trace"))
+
 ;;;; SALT MODE
 
 (use-package! salt-mode)
@@ -593,8 +846,14 @@ templates into newly created files"
       "o" #'systemd-doc-open)
 
 ;;;; TREEMACS
+;;;; treemacs
 
-(setq +treemacs-git-mode 'extended)
+(use-package treemacs
+  :defer 3
+  :custom
+  (treemacs-width 35)
+  :config
+  (setq +treemacs-git-mode 'extended))
 
 ;;;; TREESITTER
 
@@ -618,6 +877,16 @@ templates into newly created files"
         "T" #'telega
         ))
 
+;;;; UNDO
+
+(use-package undo-tree
+  :defer 0.3
+  :config
+  (setq undo-tree-auto-save-history t)
+  (setq undo-tree-history-directory-alist '(("." . "~/tmp/undo")))
+  ;; (evil-set-undo-system 'undo-tree)
+  (global-undo-tree-mode))
+
 ;;;; VLF
 
 (use-package! vlf-setup
@@ -633,6 +902,39 @@ templates into newly created files"
   :config
   (add-hook 'doom-first-buffer-hook  #'marty/startwakatime)
   (setq wakatime-cli-path "/usr/bin/wakatime"))
+
+;;;; WEB MODE
+
+(use-package web-mode
+  :defer 0.5
+  :config
+  (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+  (setq web-mode-comment-formats
+        '(("java"       . "/*")
+          ("javascript" . "//")
+          ("typescript" . "//")
+          ("vue"        . "//")
+          ("php"        . "/*")
+          ("pug"        . "//")
+          ("css"        . "/*")))
+  ;; (add-to-list 'web-mode-comment-formats '("pug" . "//"))
+  ;; (setcdr (assoc "javascript" web-mode-comment-formats) "//")
+  ;; (add-to-list 'web-mode-comment-formats '("javascript" . "//"))
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-css-indent-offset 2))
+
+;;;;; HTML
+
+(use-package emmet-mode
+  :hook ((scss-mode . emmet-mode) (css-mode . emmet-mode) (ng2-html-mode . emmet-mode) (html-mode . emmet-mode))
+  :defer 5)
+
+;;;;; JSON
+
+(use-package json-mode
+  :defer 5
+  :hook (json-mode . format-all-mode))
+
 
 ;;; TEMP / FIXUP
 ;;;; XTERM SET-WINDOW-TITLE
