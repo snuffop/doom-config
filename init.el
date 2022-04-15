3f319ba68936ddb03f811cfdc0c4feafb57abf5d
Marty Buchaus <marty@dabuke.com>
2022-04-15 17:36:30 -0400

    asdfasdf

--- init.el
+++ init.el
@@ -14,13 +14,14 @@
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       (vertico +icons)
+       (vertico
+        +icons)
 
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       doom-quit         ; DOOM quit-message prompts when you quit Emacus
        (emoji +unicode +github)
        ;; hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
